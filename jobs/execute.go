101e462fdbd4dba64e9fb0889246c9ced69ddc06
peter.yang <peter.yang@cagetest.com>
2019-03-27 15:52:42 +0800

    [feature]add execute dir

--- jobs/execute.go
+++ jobs/execute.go
@@ -11,6 +11,7 @@ import (
 type Execute struct {
 	command string
 	args    []string
+	dir     string
 	debug   bool
 }
 
@@ -26,11 +27,14 @@ func newExecute(c *Config) (*Execute, error) {
 		return nil, err
 	}
 
+	dir := c.StringDefault("dir", "")
+
 	debug := c.BoolDefault("debug", false)
 
 	return &Execute{
 		command: command,
 		args:    args,
+		dir:     dir,
 		debug:   debug,
 	}, nil
 }
@@ -42,18 +46,21 @@ func (s Execute) Do(ctx *Context) error {
 		args[index] = ctx.Expand(arg)
 	}
 
+	dir := ctx.Expand(s.dir)
+
 	if s.debug {
 		zap.L().Debug("execute command",
 			zap.String("command", s.command),
-			zap.Strings("args", args))
+			zap.Strings("args", args),
+			zap.String("dir", dir))
 	}
 
 	cmd := exec.Command(s.command, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Dir = dir
 
 	err := cmd.Run()
-	// err := cmd.Run()
 	if err != nil {
 		ee, ok := err.(*exec.ExitError)
 		if !ok {
@@ -65,14 +72,19 @@ func (s Execute) Do(ctx *Context) error {
 			return err
 		}
 
-		ee.ProcessState.ExitCode()
-
 		zap.L().Error("execute command failed",
 			zap.Error(ee),
 			// zap.ByteString("output", output),
 			zap.String("command", s.command),
 			zap.Strings("args", args))
 		return err
+	}
+
+	if s.debug {
+		zap.L().Debug("execute command success",
+			zap.String("command", s.command),
+			zap.Strings("args", args),
+			zap.String("dir", dir))
 	}
 
 	return nil
